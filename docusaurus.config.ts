04309c0a5654a815bab68bf4e62261fc146898b8
EXStevens <cstevens@expcs.net>
2025-11-26 17:54:29 +0800

    favicon.ico

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'sb.csts.dev',
+  url: 'https://sb.csts.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',

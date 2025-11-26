ee6c62966c673c317bb4034b3cd447342efd180f
Charlie Stevens <cstevens@expcs.net>
2025-11-26 07:19:39 +0000

    Add Docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/workspaces/SolutionBase/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/workspaces/SolutionBase/node_modules/infima/dist/css/default/default.css"),
+  require("/workspaces/SolutionBase/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/workspaces/SolutionBase/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/workspaces/SolutionBase/src/css/custom.css"),
+];
